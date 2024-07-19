ebe772b1dacfe904461202ead73cff96148d8428
Temirlan Temirbaev <temirlan213@bk.ru>
2024-07-19 21:14:03 +0600

    uninstall vercel deploy hook

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,12 +1,2 @@
 export default () => ({
-  "vercel-deploy" : {
-    enabled : true,
-    config: {
-      deployHook: process.env.VERCEL_PLUGIN_HOOK,
-      apiToken: process.env.VERCEL_TOKEN,
-      appFilter: process.env.VERCEL_APP_FILTER,
-      teamFilter: process.env.VERCEL_TEAM_FILTER,
-      roles: ["strapi-super-admin"],
-    },
-  }
 });
